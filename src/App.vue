c323f344c6b4576cee5442231e8877c2f08f1d04
Matteo Susanna <matteo2784@gmail.com>
2022-08-24 15:06:46 +0200

    Push

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  @import './styles/variabili_colore.scss';
+  
 
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,13 +1,84 @@
 <template>
-    <h1>ciao</h1>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="@/assets/dc-logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(link, indice) in links" :key="indice">
+                        <a :href="link.direzione">{{link.nome}}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
-    nome: 'MyHeader'
+    nome: 'MyHeader',
+    data(){
+        return{
+            links:  [
+                        {
+                            'nome': 'CHARACTERS',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'CIMICS',
+                            'direzione': '#',
+                            'attivo': true
+                        },
+                        {
+                            'nome': 'MOVIES',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'TV',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'GAMES',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'COLLECTIBLES',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'VIDEOS',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'FANS',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'NEWS',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+                        {
+                            'nome': 'SHOP',
+                            'direzione': '#',
+                            'attivo': false
+                        },
+
+                    ]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+
 
 </style>
